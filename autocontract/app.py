57a44a58cbcadc5990c8ee7050a8011c97e1740e
P3rdigas <pr.perdigao@campus.fct.unl.pt>
2024-01-11 23:07:14 +0000

    Config files for users

--- autocontract/app.py
+++ autocontract/app.py
@@ -1,3 +1,4 @@
+import configparser
 import os
 
 import customtkinter
@@ -20,6 +21,29 @@ class AutoContract(customtkinter.CTk):
         )
         self.geometry(f"{self.APP_WIDTH}x{self.APP_HEIGHT}")
         self.resizable(width=False, height=False)
+
+        # Loading config file
+        self.config_file = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "config.ini"
+        )
+        self.config = configparser.ConfigParser()
+        self.load_configuration()
+
+    def load_configuration(self):
+        if os.path.isfile(self.config_file):
+            self.config.read(self.config_file)
+            theme = self.config.get("Settings", "theme")
+
+            if theme == "system":
+                customtkinter.set_appearance_mode("system")
+            elif theme == "light":
+                customtkinter.set_appearance_mode("light")
+            else:
+                customtkinter.set_appearance_mode("dark")
+        else:
+            self.config["Settings"] = {"theme": "system"}
+            with open(self.config_file, "w") as configfile:
+                self.config.write(configfile)
 
 
 def main():
