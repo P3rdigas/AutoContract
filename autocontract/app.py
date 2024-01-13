6997fbc99b68052bf37a9beda2b18ce34882e559
P3rdigas <pr.perdigao@campus.fct.unl.pt>
2024-01-13 02:51:45 +0000

    Destination frame almost complete

--- autocontract/app.py
+++ autocontract/app.py
@@ -1,9 +1,11 @@
 import configparser
 import os
 import webbrowser
+from tkinter import filedialog
 
 import customtkinter
 from CTkMenuBar import CTkMenuBar, CustomDropdownMenu
+from CTkToolTip import CTkToolTip
 from PIL import Image
 
 
@@ -14,20 +16,21 @@ class AutoContract(customtkinter.CTk):
     TEMPLATE_FRAME_HEIGHT = 75
     DESTINATION_FRAME_HEIGHT = 75
 
+    DESTINATION_FOLDER_NAME_WIDTH = 100
+
     # Colors first correspond to Light Mode and second to Dark Mode
     MENUBAR_BACKGROUND_COLOR = "white", "black"
     DROPDOWN_BACKGROUND_COLOR = "white", "grey20"
 
     SEPARATOR_COLOR = "grey90", "black"
 
-    # DRAG_AND_DROP_BG_COLOR_LIGHT = "grey95"
-    # DRAG_AND_DROP_BG_COLOR_DARK = "grey15"
-    # DRAG_AND_DROP_SEPARATOR_COLOR_LIGHT = "grey90"
-    # DRAG_AND_DROP_SEPARATOR_COLOR_DARK = "black"
+    TOOLTIP_BORDER_COLOR = "black", "white"
 
     FRAMES_BACKGROUND_COLOR = "transparent"
 
     HOVER_COLOR = "grey85", "grey25"
+
+    NO_FOLDER_SELECTED = "No folder selected"
 
     SOURCE_CODE_URL = "https://github.com/P3rdigas/AutoContract"
 
@@ -163,12 +166,82 @@ class AutoContract(customtkinter.CTk):
             destination_frame, corner_radius=0, fg_color=self.FRAMES_BACKGROUND_COLOR
         )
 
+        # TODO: put image (number 3???)
         destination_label = customtkinter.CTkLabel(
-            destination_label_frame, text="Destination Folder"
+            destination_label_frame, text="Destination"
         )
 
         destination_controls_frame = customtkinter.CTkFrame(
             destination_frame, corner_radius=0, fg_color=self.FRAMES_BACKGROUND_COLOR
+        )
+
+        destination_folder_label = customtkinter.CTkLabel(
+            destination_controls_frame, text="Destination folder:"
+        )
+
+        self.destination_folder_name = None
+
+        # Default width of the label (can provide font),
+        self.DESTINATION_FOLDER_NAME_WIDTH = self.get_width_text(
+            self.NO_FOLDER_SELECTED
+        )
+
+        # Width: plus 1 (if width of label equals the width of the text the text you jump out by 1 px) and plus 10 (margin, 5 to the left, 5 to the right)
+        self.destination_folder_name_label = customtkinter.CTkLabel(
+            destination_controls_frame,
+            width=self.DESTINATION_FOLDER_NAME_WIDTH + 1 + 10,
+            text=self.NO_FOLDER_SELECTED,
+        )
+
+        self.destination_folder_name_label_tooltip = CTkToolTip(
+            self.destination_folder_name_label,
+            message=None,
+            corner_radius=5,
+            border_width=1,
+            border_color=self.TOOLTIP_BORDER_COLOR,
+        )
+
+        # If the text is smaller than the label width no need to show a tooltip
+        self.destination_folder_name_label_tooltip.hide()
+
+        self.destination_folder_name_label.bind(
+            "<Enter>",
+            lambda event: self.on_enter(
+                event, self.destination_folder_name_label_tooltip
+            ),
+        )
+
+        # TODO: Set a better color
+        # Width to 1 so the button takes the width of the text
+        destination_folder_button = customtkinter.CTkButton(
+            destination_controls_frame,
+            width=1,
+            text="Choose folder",
+            # fg_color="transparent",
+            hover_color=self.HOVER_COLOR,
+            command=self.choose_destination_folder,
+        )
+
+        # TODO: Set a better color
+        # Width to 1 so the button takes the width of the text
+        destination_folder_clear_button = customtkinter.CTkButton(
+            destination_controls_frame,
+            width=1,
+            text="Reset",
+            # fg_color="transparent",
+            hover_color=self.HOVER_COLOR,
+            command=self.reset_destination_folder,
+        )
+
+        # TODO: Set a better color
+        # Width to 1 so the button takes the width of the text
+        destination_generate_button = customtkinter.CTkButton(
+            destination_controls_frame,
+            width=50,
+            text="Generate",
+            # fg_color="transparent",
+            hover_color=self.HOVER_COLOR,
+            command=self.generate_contract,
         )
 
         # Template Layout
@@ -184,6 +257,12 @@ class AutoContract(customtkinter.CTk):
         separator_d_d.pack(fill="x")
 
         # Destination Layout
+        destination_folder_label.pack(side="left", padx=10)
+        # Left padding 0px and right padding 10px
+        self.destination_folder_name_label.pack(side="left", padx=(0, 10))
+        destination_folder_button.pack(side="left", padx=(0, 5))
+        destination_folder_clear_button.pack(side="left")
+        destination_generate_button.pack(side="right", padx=(0, 10))
         destination_label.pack(side="left", padx=10)
         destination_label_frame.place(x=0, y=0, relheight=0.5, relwidth=1)
         destination_controls_frame.place(x=0, rely=0.5, relheight=0.5, relwidth=1)
@@ -215,6 +294,53 @@ class AutoContract(customtkinter.CTk):
 
     def open_browser(self):
         webbrowser.open_new(self.SOURCE_CODE_URL)
+
+    def get_width_text(self, text, font=None):
+        temp_label = customtkinter.CTkLabel(self, text=text, font=font)
+        temp_label.update_idletasks()
+        width = temp_label.winfo_reqwidth()
+        temp_label.destroy()
+
+        return width
+
+    def on_enter(self, event, tooltip):
+        tooltip.get()
+
+    def choose_destination_folder(self):
+        app_path = os.path.dirname(os.path.abspath(__file__))
+
+        folder = filedialog.askdirectory(initialdir=app_path)
+
+        if folder:
+            self.destination_folder_name = folder
+
+            text_width = self.get_width_text(folder)
+
+            label_width = self.DESTINATION_FOLDER_NAME_WIDTH
+            print(label_width)
+            if text_width > self.DESTINATION_FOLDER_NAME_WIDTH:
+                # Less one (approximately three dots size in pixels)
+                text = (
+                    folder[: int(label_width / (text_width / len(folder))) - 1] + "..."
+                )
+                self.destination_folder_name_label.configure(text=text)
+                self.destination_folder_name_label_tooltip.configure(message=folder)
+
+                if self.destination_folder_name_label_tooltip.is_disabled():
+                    self.destination_folder_name_label_tooltip.show()
+            else:
+                self.destination_folder_name_label.configure(text=folder)
+                self.destination_folder_name_label_tooltip.hide()
+
+    def reset_destination_folder(self):
+        if self.destination_folder_name is not None:
+            self.destination_folder_name = None
+            self.destination_folder_name_label.configure(text=self.NO_FOLDER_SELECTED)
+            self.destination_folder_name_label_tooltip.configure(message=None)
+            self.destination_folder_name_label_tooltip.hide()
+
+    def generate_contract(self):
+        print("Generating contract")
 
 
 def main():
